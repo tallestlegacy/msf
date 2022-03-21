a02efae995f60bd59554ef8eb6b1fb7dcf60bee5
TallestLegacy <tallestlegacy@gmail.com>
2022-03-21 10:29:16 +0300

    added youtube links

---
+++ src/lib/components/YoutubeVideo.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let src;
+</script>
+
+<iframe
+	{src}
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+/>
+
+<style>
+	iframe {
+		width: 100%;
+		aspect-ratio: 16/9;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import YoutubeVideo from '$lib/components/YoutubeVideo.svelte';
+
+	const urls = [
+		'https://www.youtube.com/watch?v=WLCUxpv-TmA',
+		'https://www.youtube.com/watch?v=lGhFX4Pwj6Y',
+		'https://www.youtube.com/watch?v=W7IGTX_Peso',
+		'https://www.youtube.com/watch?v=hEUALimWs7E'
+	];
+</script>
+
+<main>
+	<h1>Market Structure Trading</h1>
+
+	<h2>Youtube Resources</h2>
+
+	<div class="youtube" />
+	{#each urls as src}
+		<YoutubeVideo {src} />
+	{/each}
+</main>
+
+<style lang="scss">
+	main {
+		padding: 2rem;
+	}
+</style>
